7b9948f31c45ba6088aca3f03e19abdc44d1ae8d
RichardSobreiro <richardsobreiro@gmail.com>
2016-06-01 14:24:26 +0200

    Add files via upload

---
+++ RegisterUser.java
@@ -0,0 +1,15 @@
+package example.richard.easymotoboy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Richard on 5/29/2016.
+ */
+public class RegisterUser extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeruser);
+    }
+}
